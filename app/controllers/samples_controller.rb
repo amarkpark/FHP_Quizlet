e3029bb1f49a23c129bf593c8d22bf8af9effce8
amarkpark <amarkpark@gmail.com>
2015-09-10 11:31:37 +0000

    have the submit/create button working on new form page ~AMP

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -7,4 +7,15 @@ class SamplesController < ApplicationController
 		@sample = Sample.new
 	end
 
+	def create
+		Sample.create(sample_params)
+		redirect_to root_path
+	end
+
+	private
+
+	def sample_params
+		params.require(:sample).permit(:comment, :author, :email)
+	end
+
 end
